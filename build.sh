4847c938988c9172eb78f829b2d0f2e5182f3185
Miguel Ángel Páez Parra <71729014+miguelparra2020@users.noreply.github.com>
2023-07-28 07:49:15 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,10 +7,3 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
-#Hola
-# Utilizar la variable de entorno para obtener la contraseña del superusuario
-SUPERUSER_PASSWORD="$MI_SUPERUSER_PASSWORD"
-
-# Crear el superusuario con la contraseña proporcionada desde la variable de entorno
-python manage.py createsuperuser --username Miguel --email mi_correo@example.com --noinput
-python manage.py changepassword Miguel "$SUPERUSER_PASSWORD"
