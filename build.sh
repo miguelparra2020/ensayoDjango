1b323854209780aa7c80a175f0af8ac2c5ca561a
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-07-28 00:03:46 -0500

    Update

--- build.sh
+++ build.sh
@@ -7,4 +7,9 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-python manage.py createsuperuser --username Miguel --email mi@example.com
+echo "Ingresa la contraseña para el superusuario:"
+read -s password
+
+# Crear un superusuario
+python manage.py createsuperuser --username Miguel --email mi_correo@example.com --noinput
+echo "$password" | python manage.py shell -c "from django.contrib.auth.models import User; user = User.objects.get(username='mi_usuario'); user.set_password('$password'); user.save()"
